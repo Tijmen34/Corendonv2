e3f2511ae5e05cf353ccd307a19f23476cf6a641
JeroendeJong <jeroen--j@hotmail.com>
2016-11-23 10:16:17 +0100

    luggage overview toegevoegd

---
+++ src/corendon/LuggageOverview.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corendon;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author JerryJerr
+ */
+public class LuggageOverview extends GridPane{
+    
+    
+    // Constructor overriden kan niet, en een eigen constructor maken ipv de originele
+    // maakt meer kapot dan je lief is, dus we schrijven een nieuwe methode om alle
+    // elementen meteen aan het scherm toe te voegen.
+    public void initScreen() {
+        
+        TextField naamKlant = new TextField();
+        
+        this.add(naamKlant, 1, 1);
+    }
+}
+
+
