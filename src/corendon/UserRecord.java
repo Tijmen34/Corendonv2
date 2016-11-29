c2e6a18f4b4dd20ca60e116d9d4b37f489358f0f
JeroendeJong <jeroen--j@hotmail.com>
2016-11-29 13:16:16 +0100

    UserRecord class

---
+++ src/corendon/UserRecord.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corendon;
+
+/**
+ *
+ * @author JerryJerr
+ */
+public class UserRecord {
+    private String userId;
+    private String userName;
+    private String firstName;
+    private String tussenVoegsel;
+    private String surName;
+    private String function;
+
+    public UserRecord(String userId, String userName, String firstName, 
+            String tussenVoegsel, String surName, String function) {
+        this.userId = userId;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.tussenVoegsel = tussenVoegsel;
+        this.surName = surName;
+        this.function = function;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getTussenVoegsel() {
+        return tussenVoegsel;
+    }
+
+    public void setTussenVoegsel(String tussenVoegsel) {
+        this.tussenVoegsel = tussenVoegsel;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public String getFunction() {
+        return function;
+    }
+
+    public void setFunction(String function) {
+        this.function = function;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRecord{" + "userId=" + userId + ", userName=" 
+                + userName + ", firstName=" + firstName + ", tussenVoegsel=" 
+                + tussenVoegsel + ", surName=" + surName + ", function=" 
+                + function + '}';
+    }
+    
+    
+    
+    
+}
+
+
