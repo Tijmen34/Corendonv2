e9cab4c59a857a1d821a5f5280307d7abd10370d
karos1 <burakinan1@hotmail.com>
2017-01-14 19:12:25 +0100

    asd

--- src/corendon/Statistics.java
+++ src/corendon/Statistics.java
@@ -9,7 +9,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Side;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -17,20 +27,89 @@ import javafx.stage.Stage;
  * @author Burak
  */
 public class Statistics extends BorderPane {
+
     ResultSet rs = null;
     PreparedStatement pst = null;
     Connection conn;
     Statement stmt;
-    private Stage primaryStage;
+    TextField wut = new TextField();
+    Button get = new Button("get");
+    double value = 0.0;
     
-        public void initScreen(Stage primaryStage) {
-            this.primaryStage = primaryStage;
-            CheckConnection();
+    public double getValues() {
+        
+        get.setOnAction(e -> {
+            
+        try {
+            
+            String SQL = "SELECT COUNT(status) AS 'Total' FROM bagage WHERE status =?";
+            pst = conn.prepareStatement(SQL);
+            pst.setString(1, "'" + wut.getText() + "'");
+            ResultSet rs = pst.executeQuery();
+            
+            if (rs.next()) {
+            double value = rs.getDouble(1);
+            System.out.println(value);
+            } else {
+                System.out.println("error");
+            }
             
             
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Error on Building Data");
+        } 
+        
+        });
+        return value;
+        
+        
+    }
+                
+    
+    public void initScreen(Stage primaryStage) {
+        
+        wut.setPromptText("Wut");
+        getValues();
+        CheckConnection();
+
+        ObservableList<PieChart.Data> pieChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Missing", getValues()),
+                        new PieChart.Data("Found", 25),
+                        new PieChart.Data("Solved", 10));
+
+        final PieChart chart = new PieChart(pieChartData);
+        chart.setTitle("Imported Fruits");
+        chart.setLabelLineLength(10);
+        chart.setLegendSide(Side.LEFT);
+
+        final Label caption = new Label("");
+        caption.setTextFill(Color.WHITE);
+        caption.setStyle("-fx-font: 16px UniSansW01-LightItalic;");
+
+        for (final PieChart.Data data : chart.getData()) {
+            data.getNode().addEventHandler(MouseEvent.MOUSE_PRESSED,
+                    new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    caption.setTranslateX(e.getSceneX() - 100);
+                    caption.setTranslateY(e.getSceneY());
+                    caption.setText(String.valueOf(data.getPieValue()) + "%");
+                }
+            });
         }
         
-        //check van tevoren de db verbinding
+        
+        this.setCenter(chart);
+        this.setTop(caption);
+        this.setBottom(wut);
+        this.setRight(get);
+        this.setMaxSize(500, 500);
+        this.setMinSize(500, 500);
+
+    }
+    
     public void CheckConnection() {
         conn = Sql.DbConnector();
         if (conn == null) {
@@ -38,4 +117,5 @@ public class Statistics extends BorderPane {
             System.exit(1);
         }
     }
+
 }
