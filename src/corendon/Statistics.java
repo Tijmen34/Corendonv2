8efdeca16edc7d26c90b2c128d520eee3c3a78fd
karos1 <burakinan1@hotmail.com>
2017-01-15 19:56:17 +0100

    chart werkt nu, maar is nog niet compleet

--- src/corendon/Statistics.java
+++ src/corendon/Statistics.java
@@ -14,12 +14,18 @@ import java.time.chrono.Chronology;
 import java.time.format.DateTimeFormatter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.Side;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
@@ -48,6 +54,7 @@ public class Statistics extends BorderPane {
     double valueSolved = 0.0;
 
     Button submitDate = new Button("Retrieve");
+    Button chooseinfo = new Button("Submit");
     Label dateChoose = new Label("Select two dates to view statistics of all cases");
     Label label1 = new Label("From: ");
     Label label2 = new Label("To: ");
@@ -55,88 +62,127 @@ public class Statistics extends BorderPane {
     DatePicker date2 = new DatePicker();
     VBox vbox = new VBox();
     HBox hbox = new HBox();
+    HBox hbox1 = new HBox();
     String pattern = "yyyy-MM-dd";
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
     String date1Formatted;
     String date2Formatted;
-    
-    public void getValues() {
-            
-            try (Connection conn = Sql.DbConnector();) {
-                
-                String SQL1 = "select count(*) total, sum(case when status = 'found' and datum_bevestiging between ? AND ? then 1 else 0 end) found, sum(case when status = 'lost' and datum_bevestiging between ? AND ? then 1 else 0 end) lost, sum(case when status = 'solved' and datum_bevestiging between ? AND ? then 1 else 0 end) solved from bagage";
-                
-                pst = conn.prepareStatement(SQL1);
-                pst.setString(1, date1Formatted);
-                pst.setString(3, date1Formatted);
-                pst.setString(5, date1Formatted);
-                pst.setString(2, date2Formatted);
-                pst.setString(4, date2Formatted);
-                pst.setString(6, date2Formatted);
-                
-                ResultSet rs = pst.executeQuery();
+    ComboBox statinfo = new ComboBox(FXCollections.observableArrayList("Pie chart", "Graph", "Both"));
 
-                if (rs.next()) {
-                    valueTotal = rs.getInt("total");
-                    valueFound = rs.getInt("found");
-                    valueMissing = rs.getInt("lost");
-                    valueSolved = rs.getInt("solved");
-                } 
-                
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                System.out.println("Error on Building Data");
+    public void getValues() {
+
+        try (Connection conn = Sql.DbConnector();) {
+
+            String SQL1 = "select count(*) total, sum(case when status = 'found' and datum_bevestiging between ? AND ? then 1 else 0 end) found, sum(case when status = 'lost' and datum_bevestiging between ? AND ? then 1 else 0 end) lost, sum(case when status = 'solved' and datum_bevestiging between ? AND ? then 1 else 0 end) solved from bagage";
+
+            pst = conn.prepareStatement(SQL1);
+            pst.setString(1, date1Formatted);
+            pst.setString(3, date1Formatted);
+            pst.setString(5, date1Formatted);
+            pst.setString(2, date2Formatted);
+            pst.setString(4, date2Formatted);
+            pst.setString(6, date2Formatted);
+
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+                valueTotal = rs.getInt("total");
+                valueFound = rs.getInt("found");
+                valueMissing = rs.getInt("lost");
+                valueSolved = rs.getInt("solved");
             }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("Error on Building Data");
+        }
+    }
+
+    public void createGraph() {
+        //defining the axes
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Number of Month");
+        //creating the chart
+        final LineChart<Number, Number> lineChart
+                = new LineChart<Number, Number>(xAxis, yAxis);
+
+        lineChart.setTitle("Total cases");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        series.setName("Test");
+        //populating the series with data
+        series.getData().add(new XYChart.Data(1, 23));
+        series.getData().add(new XYChart.Data(2, 14));
+        series.getData().add(new XYChart.Data(3, 15));
+        series.getData().add(new XYChart.Data(4, 24));
+        series.getData().add(new XYChart.Data(5, 34));
+        series.getData().add(new XYChart.Data(6, 36));
+        series.getData().add(new XYChart.Data(7, 22));
+        series.getData().add(new XYChart.Data(8, 45));
+        series.getData().add(new XYChart.Data(9, 43));
+        series.getData().add(new XYChart.Data(10, 17));
+        series.getData().add(new XYChart.Data(11, 29));
+        series.getData().add(new XYChart.Data(12, 25));
+
+        lineChart.getData().add(series);
+        this.setRight(lineChart);
+
+    }
+
+    public void createChart() {
+        ObservableList<PieChart.Data> pieChartData
+                = FXCollections.observableArrayList(
+                        new PieChart.Data("Total", valueTotal),
+                        new PieChart.Data("Missing", valueMissing),
+                        new PieChart.Data("Found", valueFound),
+                        new PieChart.Data("Solved", valueSolved));
+
+        final PieChart chart = new PieChart(pieChartData);
+        chart.setTitle("Lost & Found luggage statistics\nClick on a 'slice' to see amount");
+        chart.setLabelLineLength(10);
+        chart.setLegendSide(Side.BOTTOM);
+
+        final Label caption = new Label("");
+        caption.setTextFill(Color.BLACK);
+        caption.setStyle("-fx-font-weight: bold;");
+
+        for (final PieChart.Data data : chart.getData()) {
+            data.getNode().addEventHandler(MouseEvent.MOUSE_PRESSED,
+                    new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent e) {
+                    caption.setTranslateX(e.getSceneX() - 100);
+                    caption.setTranslateY(e.getSceneY());
+                    caption.setText(String.valueOf(data.getPieValue()) + " cases");
+                }
+            });
+        }
+        chart.setMaxSize(500, 500);
+        chart.setMinSize(500, 500);
+        this.setCenter(chart);
+        this.setTop(caption);
     }
 
     public void initScreen(Stage primaryStage) {
         
         submitDate.setOnAction(e -> {
-            
+            addStats();
             LocalDate date1Unformatted = date1.getValue();
             LocalDate date2Unformatted = date2.getValue();
             date1Formatted = formatter.format(date1Unformatted);
             date2Formatted = formatter.format(date2Unformatted);
-            
+
+            statinfo.getSelectionModel().select(1);
             getValues();
-            
-            ObservableList<PieChart.Data> pieChartData
-                    = FXCollections.observableArrayList(
-                            new PieChart.Data("Total", valueTotal),
-                            new PieChart.Data("Missing", valueMissing),
-                            new PieChart.Data("Found", valueFound),
-                            new PieChart.Data("Solved", valueSolved));
-
-            final PieChart chart = new PieChart(pieChartData);
-            chart.setTitle("Lost & Found luggage statistics\nClick on a 'slice' to see amount");
-            chart.setLabelLineLength(10);
-            chart.setLegendSide(Side.LEFT);
-            
-            final Label caption = new Label("");
-            caption.setTextFill(Color.BLACK);
-            caption.setStyle("-fx-font-weight: bold;");
-
-            for (final PieChart.Data data : chart.getData()) {
-                data.getNode().addEventHandler(MouseEvent.MOUSE_PRESSED,
-                        new EventHandler<MouseEvent>() {
-                    @Override
-                    public void handle(MouseEvent e) {
-                        caption.setTranslateX(e.getSceneX() - 100);
-                        caption.setTranslateY(e.getSceneY());
-                        caption.setText(String.valueOf(data.getPieValue()) + " cases");
-                    }
-                });
-            }
-            this.setCenter(chart);
-            this.setTop(caption);
-
+            createChart();
+            this.setBottom(hbox1);
         });
-
+        
         hbox.setStyle("-fx-background-color:#D81E05");
 
         this.setCenter(vbox);
         this.setTop(hbox);
-     
 
         hbox.getChildren().addAll(new Label(""));
         vbox.getChildren().addAll(dateChoose, label1, date1, label2, date2, submitDate);
@@ -151,4 +197,22 @@ public class Statistics extends BorderPane {
 
     }
 
-}
+    public void addStats() {
+        
+        hbox1.getChildren().addAll(statinfo, chooseinfo);
+        
+        chooseinfo.setOnAction(e -> {
+        
+            if (statinfo.getSelectionModel().getSelectedIndex() == 1) {
+                createChart();
+            } else if (statinfo.getSelectionModel().getSelectedIndex() == 2) {
+                createGraph();
+            } else if (statinfo.getSelectionModel().getSelectedIndex() == 3) {
+                createChart();
+                createGraph();
+            }
+        });
+                }
+    }
+
+
